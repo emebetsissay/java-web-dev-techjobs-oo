13bc343b0d4246f793ce09f95154355d7227b49f
emebetsissay <61718737+emebetsissay@users.noreply.github.com>
2020-05-26 18:07:14 -0500

    Add files via upload

---
+++ Employer.java
@@ -0,0 +1,16 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public class Employer extends JobField {
+
+    public Employer(String value){
+        super (value);
+    }
+
+}
+
+
+
+
+
